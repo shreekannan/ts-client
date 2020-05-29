81625207543fbd896014ac26aa5751d81f6b1566
Alex Sorafumo <alex@yuion.net>
2020-05-29 16:56:31 +1000

    fix(repo): add method for listing interface repos

--- src/http/services/repositories/repositories.service.ts
+++ src/http/services/repositories/repositories.service.ts
@@ -1,4 +1,6 @@
+import { toQueryString } from '../../../utilities/api.utilities';
 import { HashMap } from '../../../utilities/types.utilities';
+import { HttpError } from '../../http.interfaces';
 import { EngineHttpClient } from '../../http.service';
 import { EngineDriverDetails } from '../drivers/drivers.interfaces';
 import { EngineResourceService } from '../resources/resources.service';
@@ -13,6 +15,34 @@ export class EngineRepositoriesService extends EngineResourceService<EngineRepos
         ServiceManager.setService(EngineRepository, this);
         this._name = 'Repository';
         this._api_route = 'repositories';
+    }
+
+    /**
+     * Get a list of all the interfaces
+     * @param query_params Addition query parameters to pass to the request
+     */
+    public async listInterfaces(query_params: HashMap = {}): Promise<HashMap<string>> {
+        const query = toQueryString(query_params);
+        const key = `interfaces|${query}`;
+        /* istanbul ignore else */
+        if (!this._promises[key]) {
+            this._promises[key] = new Promise((resolve, reject) => {
+                const url = `${this.api_route}/interfaces${query ? '?' + query : ''}`;
+                let result: any;
+                this.http.get(url).subscribe(
+                    (d: HashMap) => result = d,
+                    (e: HttpError) => {
+                        reject(e);
+                        delete this._promises[key];
+                    },
+                    () => {
+                        resolve(result);
+                        this.timeout(key, () => delete this._promises[key], 1000);
+                    }
+                );
+            });
+        }
+        return this._promises[key];
     }
 
     /**
