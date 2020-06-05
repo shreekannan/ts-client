e97448a5f610b26ea535e9c28de6871ab8d898e6
Alex Sorafumo <alex@yuion.net>
2020-06-05 12:23:24 +1000

    chore(repos): add branch field to repositories

--- src/http/services/repositories/repository.class.ts
+++ src/http/services/repositories/repository.class.ts
@@ -23,6 +23,8 @@ export class EngineRepository extends EngineResource<EngineRepositoriesService> 
     public readonly description: string;
     /** URI that the repository can be pulled from */
     public readonly uri: string;
+    /** Working branch for the repository */
+    public readonly branch: string;
     /** Hash of the commit at the head of the repository */
     public readonly commit_hash: string;
     /** Repository type */
@@ -40,6 +42,7 @@ export class EngineRepository extends EngineResource<EngineRepositoriesService> 
         this.folder_name = raw_data.folder_name || '';
         this.description = raw_data.description || '';
         this.uri = raw_data.uri || '';
+        this.branch = raw_data.branch || 'master';
         this.commit_hash = raw_data.commit_hash || '';
         this.repo_type = typeof raw_data.repo_type === 'string' ? raw_data.repo_type as any : EngineRepositoryType.Driver;
     }
