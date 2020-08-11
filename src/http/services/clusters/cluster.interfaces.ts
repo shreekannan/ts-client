8ed80d3f9fa368b65892a7ff6b73db193fd7cdbc
MrYuion <MrYuion@users.noreply.github.com>
2020-08-11 00:30:48 +0000

    Automated deployment: Tue Aug 11 00:30:48 UTC 2020 12696ab67d10f6ab200aea9d51ccdd9db22e7f88

--- src/http/services/clusters/cluster.interfaces.ts
+++
@@ -1,9 +0,0 @@
-/** Mapping of available query paramters for the application index endpoint */
-export interface EngineClusterQueryOptions {
-    /** Whether the cluster status details should be included */
-    include_status?: string;
-    /** Number of results to return. Defaults to `20`. Max `500` */
-    limit?: number;
-    /** Offset the page of results. Max `10000` */
-    offset?: number;
-}
--- src/http/services/drivers/drivers.enums.ts
+++
@@ -1,8 +0,0 @@
-/** Allowed roles for Engine Modules */
-export enum EngineDriverRole {
-    SSH = 0,
-    Device = 1,
-    Service = 2,
-    Websocket = 3,
-    Logic = 99
-}
